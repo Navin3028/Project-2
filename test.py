e6ac94c57567d1c9a3dfeec52c28b9ae6ef2e619
NAVEEN S <115558879+Navin3028@users.noreply.github.com>
2024-09-25 09:28:09 +0000

    Update test.py

--- test.py
+++ test.py
@@ -1,20 +1,10 @@
-import os
-from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
-from cryptography.hazmat.backends import default_backend
-from cryptography.hazmat.primitives.asymmetric import rsa, padding
-from cryptography.hazmat.primitives import hashes
-
-# Example for a symmetric cipher: AES
-aes = Cipher(algorithms.AES(key), modes.CBC(iv), backend=default_backend())  # Noncompliant
-aes = Cipher(algorithms.AES(key), modes.ECB(), backend=default_backend())  # Noncompliant
-
-# Example for a asymmetric cipher: RSA
-ciphertext = public_key.encrypt(
-  message,
-  padding.PKCS1v15() # Noncompliant
-)
-
-plaintext = private_key.decrypt(
-  ciphertext,
-  padding.PKCS1v15() # Noncompliant
-)
+DATABASES = {
+    'postgresql_db': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'quickdb',
+        'USER': 'sonarsource',
+        'PASSWORD': '', # Noncompliant
+        'HOST': 'localhost',
+        'PORT': '5432'
+    }
+}
