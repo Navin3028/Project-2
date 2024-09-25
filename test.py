d0245cfc2af8a97d5fbc1e0e10283ec7cd21c667
NAVEEN S <115558879+Navin3028@users.noreply.github.com>
2024-09-25 09:31:39 +0000

    Update test.py

--- test.py
+++ test.py
@@ -1,10 +1,7 @@
-DATABASES = {
-    'postgresql_db': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'quickdb',
-        'USER': 'sonarsource',
-        'PASSWORD': '', # Noncompliant
-        'HOST': 'localhost',
-        'PORT': '5432'
-    }
-}
+import ssl
+
+ctx1 = ssl._create_unverified_context() # Noncompliant: by default certificate validation is not done
+ctx2 = ssl._create_stdlib_context() # Noncompliant: by default certificate validation is not done
+
+ctx3 = ssl.create_default_context()
+ctx3.verify_mode = ssl.CERT_NONE # Noncompliant
